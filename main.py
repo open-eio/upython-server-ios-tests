4e5f18285758e556486c336e4b8ca6bd2f4a08ab
Craig Versek <cversek+github@gmail.com>
2017-01-29 01:42:49 -0500

    Update main.py
    always close socket when finished

--- main.py
+++ main.py
@@ -22,19 +22,22 @@ import sockets
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
-s.bind(addr)
-s.listen(1)
-print('listening on', addr)
+try:
+    s.bind(addr)
+    s.listen(1)
+    print('listening on', addr)
 
-while True:
-    cl, addr = s.accept()
-    print('client connected from', addr)
-    cl_file = cl.makefile('rwb', 0)
     while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-    response = html #FIMXE% '\n'.join(rows)
-    cl.send(response)
-    cl.close()
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+        response = html #FIMXE% '\n'.join(rows)
+        cl.send(response)
+        cl.close()
+finally:
+    s.close()
